2a2dc95150776467349f343318814867dacd56ce
dvatsav <deepak.srivatsav@gmail.com>
2017-10-27 21:58:12 +0530

    distance comparison

--- deepak_store_ID.py
+++ deepak_store_ID.py
@@ -7,15 +7,48 @@ import requests
 import json
 import pickle
 import logging
-
+from math import radians, cos, sin, asin, sqrt
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 					level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 LOCATION, BIO = range(2)
-
+DISTANCE_THRESHOLD = 5
 database = {}
+
+def find_partner(user_location, user_chat_id):
+	try:
+		partner_list = []
+		current_user_dictionary = database
+		# read dictionary from pickle
+		for i in current_user_dictionary:
+			if i != user_chat_id:
+				print (i, user_chat_id)
+				coordinate_dictionary = {}
+				inner_dictionary = current_user_dictionary[i]
+				coordinates = inner_dictionary['coordinates']
+				coordinate_dictionary["longitude"] = coordinates[1]
+				coordinate_dictionary["latitude"] = coordinates[0]
+				if checkDistance(user_location, coordinate_dictionary) < DISTANCE_THRESHOLD:
+					partner_list.append(inner_dictionary)
+
+		print ("list is: ", partner_list)
+	except:
+		print ("list is empty")
+	return partner_list
+
+def checkDistance(user_location, other_user_location):
+	user_lon = user_location["longitude"]
+	user_lat = user_location["latitude"]
+	other_lon = other_user_location["longitude"]
+	other_lat = other_user_location["latitude"]
+
+	lon1, lat1, lon2, lat2 = map(radians, [user_lon, user_lat, other_lon, other_lat])
+
+	km = 6371*(2*asin(sqrt(sin((lat2 - lat1)/2)**2 + cos(lat1) * cos(lat2) * sin((lon2 - lon1)/2)**2)))
+	print ("The distance between the two points is: " + str(km))
+	return km
 
 def start(bot, update):
 	print (update.message.chat.id)
@@ -39,7 +72,7 @@ def location(bot, update):
 	database[update.message.chat.id]['first_name'] = user.first_name
 	database[update.message.chat.id]['username'] = user.username
 	database[update.message.chat.id]['coordinates'] = (user_location.latitude, user_location.longitude)
-
+	find_partner(user_location, update.message.chat.id)
 	update.message.reply_text('Maybe I can visit you sometime! '
 							  'At last, What sort of activity interests you?', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
@@ -63,6 +96,7 @@ def bio(bot, update):
 	logger.info("Bio of %s: %s", user.first_name, update.message.text)
 	update.message.reply_text('Thank you! I hope we can talk again some day.')
 	print (database)
+	
 	return ConversationHandler.END
 	
 
@@ -118,11 +152,10 @@ def main():
 
 
 if __name__ == '__main__':
-	
 	try:
 		database = pickle.load(open("database.pickle", 'rb'))
 		print (database)
-	except EOFError:
+	except:
 		database = {}
 	main()
 
